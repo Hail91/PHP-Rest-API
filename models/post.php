04020b1aecf5b1165ab16fda3115831028d2590e
Aaron <aarongillies88@gmail.com>
2020-09-29 20:25:06 -0400

    setup basic post class with query to read posts

--- models/post.php
+++ models/post.php
@@ -1,32 +1,38 @@
-<?php
-    class Post {
-        private $conn;
-        private $table = 'posts';
+<?php 
+  class Post {
+    private $conn;
+    private $table = 'posts';
 
-        // Post Properties
-        public $id;
-        public $category_id;
-        public $category_name; // This will be achieved through a table join.
-        public $title;
-        public $body;
-        public $author;
-        public $created_at;
+    // Post Properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
 
-        // Need to set a constructor that will initialize this class with a Database connection
-        public function __construct($db) { // Accept a database as a parameter
-            this->conn = $db; // Then set that to our connection property for this class.
-        }
-
-        // CRUD methods below
-        public function get_posts() {
-            $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
-            FROM ' . $this->table . ' p LEFT_JOIN Categories c ON p.category_id = c.id ORDER_BY p.created_at DESC';
-
-            // Need to create prepared statement
-            $stmt = $this->conn->prepare($query)
-            // Execute query
-            $stmt->execute()
-            // Return stmt
-            return $stmt;
-        }
+    // Constructor 
+    public function __construct($db) {
+      $this->conn = $db;
     }
+
+    // Get Posts
+    public function get_posts() {
+      // Create query
+      $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+                                FROM ' . $this->table . ' p
+                                LEFT JOIN
+                                  categories c ON p.category_id = c.id
+                                ORDER BY
+                                  p.created_at DESC';
+      
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+      // Return Statement
+      return $stmt;
+    }
+}
