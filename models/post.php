4cde0af038ff8917fc4cfbbb9674273136a8de2b
Aaron <aarongillies88@gmail.com>
2020-10-05 16:53:30 -0400

    updated Post model file with post creation function, just need to add update and delete functionality

--- models/post.php
+++ models/post.php
@@ -1,67 +1,71 @@
-<?php 
-  class Post {
-    private $conn;
-    private $table = 'posts';
+<?php
+class Post
+{
+  private $conn;
+  private $table = 'posts';
 
-    // Post Properties
-    public $id;
-    public $category_id;
-    public $category_name;
-    public $title;
-    public $body;
-    public $author;
-    public $created_at;
+  // Post Properties
+  public $id;
+  public $category_id;
+  public $category_name;
+  public $title;
+  public $body;
+  public $author;
+  public $created_at;
 
-    // Constructor 
-    public function __construct($db) {
-      $this->conn = $db;
-    }
+  // Constructor 
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
 
-    // Get Posts
-    public function get_posts() {
-      // Create query
-      $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+  // Get Posts
+  public function get_posts()
+  {
+    // Create query
+    $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
                                 FROM ' . $this->table . ' p
                                 LEFT JOIN
                                   categories c ON p.category_id = c.id
                                 ORDER BY
                                   p.created_at DESC';
-      
-      // Prepare statement
-      $stmt = $this->conn->prepare($query);
 
-      // Execute query
-      $stmt->execute();
-      // Return Statement
-      return $stmt;
-    }
-    
-    // Function to retrieve single Post
-    public function get_single_post() {
-      // Create query
-      $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Execute query
+    $stmt->execute();
+    // Return Statement
+    return $stmt;
+  }
+  // Function to retrieve single Post
+  public function get_single_post()
+  {
+    // Create query
+    $query = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
                                 FROM ' . $this->table . ' p
                                 LEFT JOIN
                                   categories c ON p.category_id = c.id
                                 WHERE
                                   p.id = ?
                                 LIMIT 0,1';
-      // Prepare statement
-      $stmt = $this->conn->prepare($query);
-      // Bind post id
-      $stmt->bindParam(1, $this->id);
-      // Execute query
-      $stmt->execute();
-      $row = $stmt->fetch(PDO::FETCH_ASSOC);
-      // Set properties
-      $this->title = $row['title'];
-      $this->body = $row['body'];
-      $this->author = $row['author'];
-      $this->category_id = $row['category_id'];
-      $this->category_name = $row['category_name'];
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+    // Bind post id
+    $stmt->bindParam(1, $this->id);
+    // Execute query
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Set properties
+    $this->title = $row['title'];
+    $this->body = $row['body'];
+    $this->author = $row['author'];
+    $this->category_id = $row['category_id'];
+    $this->category_name = $row['category_name'];
   }
   // Function to add a post to the database
-  public function add_post() {
+  public function add_post()
+  {
     $query = 'INSERT INTO ' . $this->table . ' 
     SET
       title = :title,
@@ -69,7 +73,7 @@
       author = :author,
       category_id = :category_id';
     // Prepare the statement
-    $stmt = $this->conn->prepare($query)
+    $stmt = $this->conn->prepare($query);
     // Probably should 'clean' the data since it's coming from the user.
     $this->title = htmlspecialchars(strip_tags($this->title));
     $this->body = htmlspecialchars(strip_tags($this->body));
@@ -77,18 +81,21 @@
     $this->category_id = htmlspecialchars(strip_tags($this->category_id));
 
     // Need to Bind data
-    $stmt->bindParam('title', $this->title)
-    $stmt->bindParam('body', $this->body)
-    $stmt->bindParam('author', $this->author)
-    $stmt->bindParam('category_id', $this->category_id)
+    $stmt->bindParam('title', $this->title);
+    $stmt->bindParam('body', $this->body);
+    $stmt->bindParam('author', $this->author);
+    $stmt->bindParam('category_id', $this->category_id);
 
     // Execute the query
-    if($stmt->execute()) {
+    if ($stmt->execute()) {
       return true;
     }
 
     // If there is an error
-    printf("Error: %s.\n", $stmt->error)
+    printf("Error: %s.\n", $stmt->error);
     return false;
   }
+
+  // Need to add update post function
+  // Need to add delete post function
 }
